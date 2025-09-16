e03db5c7c440f1ff344ed02a7d404235f959dd91
Exo. <fx.bergeron011@gmail.com>
2025-09-16 13:20:38 -0400

    Patch langue 0.2

--- i18n.ts
+++ i18n.ts
@@ -3,8 +3,8 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 // Import des fichiers de traduction
-import en from "./locales/en.json";
-import fr from "./locales/fr.json";
+import en from "./src/locales/en.json";
+import fr from "./src/locales/fr.json";
 
 i18n
   .use(LanguageDetector) // détecte automatiquement (navigateur ou localStorage)
