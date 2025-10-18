5a2edc0facc7c4b10646704babc33b5b3a33f4bd
Exo. <143846518+whyyexo@users.noreply.github.com>
2025-10-18 11:27:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { UserDataProvider } from './contexts/UserDataContext';
 import { LoadingProvider } from './contexts/LoadingContext';
 import { CurrencyProvider } from './contexts/CurrencyContext';
@@ -124,19 +125,21 @@ function AppContent() {
 function App() {
   return (
     <AppErrorBoundary>
-      <Router>
-        <AnalyticsWrapper>
-          <LoadingProvider>
-            <CurrencyProvider>
-              <UserDataProvider>
-                <AppContent />
-              </UserDataProvider>
-            </CurrencyProvider>
-          </LoadingProvider>
-        </AnalyticsWrapper>
-      </Router>
-      {/* EmergencyFallback temporairement désactivé pour debugging */}
-      {/* <EmergencyFallback /> */}
+      <AuthProvider>
+        <Router>
+          <AnalyticsWrapper>
+            <LoadingProvider>
+              <CurrencyProvider>
+                <UserDataProvider>
+                  <AppContent />
+                </UserDataProvider>
+              </CurrencyProvider>
+            </LoadingProvider>
+          </AnalyticsWrapper>
+        </Router>
+        {/* EmergencyFallback temporairement désactivé pour debugging */}
+        {/* <EmergencyFallback /> */}
+      </AuthProvider>
     </AppErrorBoundary>
   );
 }
